8875b6ffa1dc8fc054c79269af04335eaf63cd06
clemhff <clemthebest57@gmail.com>
2021-01-25 12:15:04 +0100

    interval 0.9s

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {insertNewTicker} = require('./insertNewTicker');
 //insertNewTicker('adadt');
 
 let timerId = setInterval(() => {
-  console.log("running a task every 5s");
+  console.log("running a task every 0.9s");
   insertNewTicker('adausdt', 120000);
   insertNewTicker('ethusdt', 240000);
-}, 5000);
+}, 900);
