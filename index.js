72d3e86cc82a487b525d4b103f8d391356f16a76
clemhff <clemthebest57@gmail.com>
2021-02-02 20:20:49 +0100

    optim delay

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const {insertNewTicker} = require('./insertNewTicker');
 
 let timerId = setInterval(() => {
   console.log("running a task every 0.9s");
-  insertNewTicker('adausdt', 120000);
-  insertNewTicker('ethusdt', 240000);
+  insertNewTicker('adausdt', 70000);
+  insertNewTicker('ethusdt', 80000);
 }, 900);
